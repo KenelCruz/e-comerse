7e79ae721452fda432393d2afb49e0beb41001b2
kenelcruz <kenelcruz@gmail.com>
2025-01-22 18:42:08 -0400

    add: card-categories component

---
+++ src/app/shared/component/card-categories/card-categories.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-categories',
+  imports: [],
+  templateUrl: './card-categories.component.html',
+  styleUrl: './card-categories.component.css'
+})
+export class CardCategoriesComponent {
+
+}
