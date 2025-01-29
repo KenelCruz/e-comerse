cbb5031ee8aa7b5493e31a754c064070b068bba5
kenelcruz <kenelcruz@gmail.com>
2025-01-29 02:14:55 -0400

    add: card-product.component

--- src/app/shared/component/card-product/card-product.component.ts
+++ src/app/shared/component/card-product/card-product.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-card-product',
-  imports: [],
+  standalone: false,
   templateUrl: './card-product.component.html',
   styleUrl: './card-product.component.css'
 })
