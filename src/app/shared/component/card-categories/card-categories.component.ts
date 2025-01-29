dc60515a7e505b5e1a83a5607c0db07f75ba3f5e
kenelcruz <kenelcruz@gmail.com>
2025-01-29 02:14:03 -0400

    add: card-categories.component

--- src/app/shared/component/card-categories/card-categories.component.ts
+++ src/app/shared/component/card-categories/card-categories.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-card-categories',
-  imports: [],
+  standalone: false,
   templateUrl: './card-categories.component.html',
   styleUrl: './card-categories.component.css'
 })
 export class CardCategoriesComponent {
-
+  @Input() title: string = 'Fregances';
+  @Input() description: string = 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s ' ;
 }
