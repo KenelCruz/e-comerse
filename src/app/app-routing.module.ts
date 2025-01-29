e7f086956bde6c9a94766782d569f8f57fe835d3
kenelcruz <kenelcruz@gmail.com>
2025-01-29 02:13:37 -0400

    add: routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {UserRoutingModule} from './features/user/user-routing.module';
 
 
 const routes: Routes = [
@@ -7,7 +8,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes), ],
+  imports: [RouterModule.forRoot(routes), UserRoutingModule ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,45 @@
-import { Routes } from '@angular/router';
+import {RouterModule, Routes} from '@angular/router';
+import {HomeComponent} from './features/home/home.component';
+import {NgModule} from '@angular/core';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+    loadChildren: () => import('./features/home/home.module').then(m => m.HomeModule),
+    pathMatch: 'full',
+  },
+  {
+    path: 'products',
+    loadChildren: () => import('./features/products/products.module').then(m => m.ProductsModule),
+  },
+  {
+    path: 'cart',
+    loadChildren: () => import('./features/cart/cart.module').then(m => m.CartModule),
+  },
+  {
+    path: 'checkout',
+    loadChildren: () => import('./features/checkout/checkout.module').then(m => m.CheckoutModule),
+  },
+  {
+    path: 'user',
+    loadChildren: () => import('./features/user/user.module').then(m => m.UserModule),
+  },
+  {
+    path: '**',
+    redirectTo: 'home',
+  }
+
+];
+
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
